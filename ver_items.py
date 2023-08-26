af748fdf4b06fdfb26b15800294dbe09c96a8407
ThaysonScript <thaysonguedes01@gmail.com>
2023-08-26 17:14:36 -0300

    docs - ver_items

--- ver_items.py
+++ ver_items.py
@@ -24,23 +24,22 @@ def visualizar_item():
         
         for dado in dados:
             fabricante, modelo, serial = dado.strip().split(',')
-            
             dados_totais.append([fabricante, modelo, serial])
             
-            
+        # 2.3.1. busca por nome do item
         if escolha == '1':
             nome_fabricante_buscar = input('Digite o nome da fabricante do seu item cadastrado: ')
             
             for dado in dados_totais:
                 fabricante, modelo, serial = dado
             
-                #  2.3.1. busca por nome do item
+                #  verificar se o nome da fabricante de entrada é igual aos dados cadastrados no dados.txt
                 if nome_fabricante_buscar == fabricante:
                     print(f'Seu item já esta cadastrado com sucesso com o nome da fabricante = {fabricante}')
                     
-                    # 2.3.2. busca por propriedade do item
+                    # 2.3.2. busca por propriedade do item      !( a partir do nome da fabricante ja lido de entrada )!
                     print('Deseja ver as propriedades desse item? ')
-                    deseja_ver = input(f'Digite "s" ou "n" para consultar as propriedades da fabricante: {fabricante}').lower()
+                    deseja_ver = input(f'Digite "s" ou "n" para consultar as propriedades da fabricante {fabricante}: ').lower()
                     
                     if deseja_ver == 's':
                         '''pergunta:
@@ -53,39 +52,43 @@ def visualizar_item():
                         # nao buscar item
                         break
                     
-                    else:
-                        print('Terminando consulta....')
                         
-                        
+        #  2.3.2. busca por propriedade do item
         elif escolha == '2':
             menu_propriedades_busca()
+            
             buscar_item_por_propriedade = input('Digite alguma das opções acima: ')
             
+            # buscar por modelo e retornar fabricante ou serial do item ja cadastrado
             if buscar_item_por_propriedade == '1':
-                digitar_modelo = input('Digite o modelo do seu item cadastrado: ')
                 
+                digitar_modelo = input('Digite o modelo do seu item cadastrado: ')
                 deseja_ver_oque = input('Digite o que voce deseja ver a partir do seu modelo digitado ( fabricante ou serial ): ')
                 
                 for dado in dados_totais:
                     fabricante, modelo, serial = dado
                     
+                    # retornar fabricante do modelo do item ja cadastrado
                     if deseja_ver_oque == 'fabricante' and digitar_modelo == modelo:
                         print(f'A fabricante do seu item com modelo = {digitar_modelo} é = {fabricante}')
                         
+                    # retornar serial do modelo do item ja cadastrado
                     elif deseja_ver_oque == 'serial' and digitar_modelo == modelo:
                         print(f'O serial/codigo do seu item com modelo = {digitar_modelo} é = {serial}')
                         
-                        
+            # buscar por serial/codigo do item ja cadastrado e retornar fabricante ou modelo          
             elif buscar_item_por_propriedade == '2':
-                digitar_serial = input('Digite o serial/codigo do seu item cadastrado: ')
                 
+                digitar_serial = input('Digite o serial/codigo do seu item cadastrado: ')
                 deseja_ver_oque = input('Digite o que voce deseja ver a partir do seu modelo digitado ( fabricante ou modelo ): ')
                 
                 for dado in dados_totais:
                     fabricante, modelo, serial = dado
                     
+                    # retornar fabricante a partir do serial
                     if deseja_ver_oque == 'fabricante' and digitar_serial == serial:
                         print(f'A fabricante do seu item com serial = {digitar_serial} é = {fabricante}')
                         
+                    # retornar modelo a partir do serial
                     elif deseja_ver_oque == 'modelo' and digitar_serial == serial:
                         print(f'O modelo do seu item com serial = {digitar_serial} é = {modelo}')

106cd50eca4c282d586a23bed56bb8f4c2ef3f15
ThaysonScript <thaysonguedes01@gmail.com>
2023-08-31 22:22:06 -0300

    add/update - deletar_item/atualizar_item

--- modulos_projeto/atualizar_item.py
+++ modulos_projeto/atualizar_item.py
@@ -1,17 +1,27 @@
-def dados_armazenados():
+# CARREGAR DADOS CADASTRADOS
+def carregar_dados():
     with open('dados.csv', 'r') as dados:
         linhas = dados.readlines()
     return linhas
 
-def novos_dados(nova_fabricante: str) -> str:
+# REGISTRAR DADOS TOTAIS (ATUALIZADOS E ANTIGOS)
+def salvar_dados(dados_atualizados):
+    with open('dados.csv', 'w') as dados:
+        dados.writelines(dados_atualizados)
+
+
+# ENTRADA PARA NOVOS DADOS ATUALIZADOS E O RETORNO DELES
+def novos_dados() -> str:
     nova_fabricante = input('Digite a nova fabricante: ').strip()
     novo_modelo = input('Digite o novo modelo: ').strip()
     novo_serial = input('Digite o novo serial: ').strip()
     
     return f'{nova_fabricante},{novo_modelo},{novo_serial}\n'
 
+
+# PROCURAR DADO A ATUALIZAR
 def atualizar_dados():
-    dados_cadastrados = dados_armazenados()
+    dados_cadastrados = carregar_dados()
     print(f'Items disponiveis: \n{dados_cadastrados}')
     
     item_atualizar = input('Digite o nome do item a ser atualizado: ')
@@ -20,15 +30,10 @@ def atualizar_dados():
         fabricante, modelo, serial = linha.strip().split(',')
         
         if item_atualizar == fabricante:
-            novos_items = novos_dados(fabricante)
+            novos_items = novos_dados()
             linhas_atualizadas.append(novos_items)
             
         else:
             linhas_atualizadas.append(f'{fabricante},{modelo},{serial}\n')
         
     salvar_dados(linhas_atualizadas)
-    
-    
-def salvar_dados(dados_atualizados):
-    with open('dados.csv', 'w') as dados:
-        dados.writelines(dados_atualizados)
--- modulos_projeto/deletar_item.py
+++ modulos_projeto/deletar_item.py
@@ -1,6 +1,15 @@
+# REGISTRAR A EXCLUSAO DE UM DADO
+def registrar_delete(dados_armazenados):
+    with open("dados.csv", "w") as arquivo_csv:
+        for dado in dados_armazenados:
+            arquivo_csv.writelines(dado)
+
+
+# CARREGAR DADOS CADASTRADOS E PROCURAR DADO ESPECIFICO PARA EXCLUIR
 def deletar_item():
     deletar_item = input("Digite o nome do item que deseja excluir: ")
     linhas_atualizadas = []
+    
     with open("dados.csv", "r") as dados:
         for linha in dados:
             fabricante, modelo, serial = linha.strip().split(",")
@@ -8,13 +17,6 @@ def deletar_item():
             if fabricante != deletar_item:
                 linhas_atualizadas.append(f'{fabricante},{modelo},{serial}\n')
                 
-    print(f"Registro com o nome '{deletar_item}' foi excluído.")
-                
+    print(f"Registro com o nome '{deletar_item}' foi excluído.")  
+           
     registrar_delete(linhas_atualizadas)
-
-
-def registrar_delete(dados_armazenados):
-
-    with open("dados.csv", "w") as arquivo_csv:
-        for dado in dados_armazenados:
-            arquivo_csv.writelines(dado)
